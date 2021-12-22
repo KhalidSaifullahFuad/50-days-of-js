83301118678bd782c8262919498fc7db6e0aa8d5
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-22 23:10:32 +0600

    ✅  Added Day#2 Challenge

---
+++ Day #2.js
@@ -0,0 +1,11 @@
+//Write a program to reverse a string
+/* 
+* String can be reversed by iterating it and storing it in reverse order
+* String can also be reversed by converting it to array, then joining it after reversing 
+*/
+
+
+// Test Cases
+reverseAString("JavaScript is awesome") should return "emosewa si tpircSavaJ"
+reverseAString("Peter Parker is Spiderman") should return "namredipS si rekraP reteP"
+reverseAString("codedamn") should return "nmadedoc"

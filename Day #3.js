ce0439e037a5e2cdc4594d0adfddedbabc4d6b4b
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-22 23:42:17 +0600

    ✅  Added Day#3 Challenge

---
+++ Day #3.js
@@ -0,0 +1,12 @@
+// Write a program to reverse a given integer number
+/*
+* The remainder of the number can be fetched and the number can be divided by 10 to remove the the digit in loop till number becomes 0.
+* A simple approach to reverse a number could also be to convert it in to a string and then reverse it.
+*/
+
+// Solution
+
+// Test Cases
+console.log(reverseGivenInteger(3849));// returns 9483
+console.log(reverseGivenInteger(2222));// returns 2222
+console.log(reverseGivenInteger(1002));// returns 2001

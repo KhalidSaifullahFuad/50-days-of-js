f8b7accf8d53a32ca6442dd38c7b2069b4ffaf74
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-23 23:06:22 +0600

    ✨ Updated Day#1 solution

--- Day #1.js
+++ Day #1.js
@@ -1,13 +1,14 @@
 // Function which returns a random number in the given range
-/* Create a function which returns a random number in the given range of values both inclusive */
-
-// Test Cases 
-console.log(randomNumberGeneratorInRange(10, 50)); // should return a number between 10-50 (inclusive)
-console.log(randomNumberGeneratorInRange(100, 500)); // should return a number between 100-500 (inclusive)
+/*
+*  Create a function which returns a random number in the given range of values both inclusive
+*/
 
 // solution
 function randomNumberGeneratorInRange(rangeStart, rangeEnd) {
 	return new Date().getTime()%rangeEnd+rangeStart;
 }
 
+// Test Cases 
+console.log(randomNumberGeneratorInRange(10, 50)); // should return a number between 10-50 (inclusive)
+console.log(randomNumberGeneratorInRange(100, 500)); // should return a number between 100-500 (inclusive)
 

3ad9c92a785162f04ab5d178e1c6899d26fbce3b
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-23 23:44:28 +0600

    ✅  Added challenge Day#6

---
+++ Day #6.js
@@ -0,0 +1,9 @@
+//Write a function to truncate a string to a certain number of words
+/*
+* Truncate a string to a certain number of words
+*/
+
+
+// Test Cases
+console.log(truncateWithWordLimit("JavaScript is simple", 3); // returns "JavaScript is simple"
+console.log(truncateWithWordLimit("Codedamn is the best place to learn to code", 5); // returns "Codedamn is the best place"

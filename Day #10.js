82fe114c04a0c3f6bb20f8984dc15056bf5a119c
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-26 23:34:40 +0600

    ✅ Updated Day#10 challenge

--- Day #10.js
+++ Day #10.js
@@ -7,3 +7,4 @@
 // Test Cases
 removeArrayElement("money"); // returns the array without the money object
 removeArrayElement("id"); // returns the array without the id object
+removeArrayElement("cStatus"); // returns the array without the cStatus object

cb6844196a156dc87cdd0d1dc6d4fdb87b45fec7
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-26 23:33:53 +0600

    ✅  Added Day#10 challenge

---
+++ Day #10.js
@@ -0,0 +1,9 @@
+// Write a function to remove array element based on object property?
+/*
+* How to remove array element based on object property?
+*/
+
+
+// Test Cases
+removeArrayElement("money"); // returns the array without the money object
+removeArrayElement("id"); // returns the array without the id object

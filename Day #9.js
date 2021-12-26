d78a17d8725297262732a0c009297c324649c8d9
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-26 23:31:39 +0600

    ✅ Added Challenge Day#9

---
+++ Day #9.js
@@ -0,0 +1,9 @@
+// Write a function to check if an object is empty or not in javaScript?
+/*
+* How to check if an object is empty or not in javaScript?
+*/
+
+// Test Cases
+
+isEmpty({}); // returns true
+isEmpty({key: 1}); // returns false

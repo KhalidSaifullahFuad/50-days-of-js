d79583b3150ae7d3b0728d771fe8bc571509d484
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-26 23:41:48 +0600

    ✅  Added Day#11 challenge

---
+++ Day #11.js
@@ -0,0 +1,7 @@
+// Return the N-th value of the Fibonacci sequence
+/*
+* Return the N-th value of the Fibonacci sequence
+*/
+
+// Test Cases
+function fibonacci(n); // returns the N-th value of the Fibonacci sequence

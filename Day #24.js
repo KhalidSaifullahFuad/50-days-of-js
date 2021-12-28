5c2ba31cff30f3b9d75fcda718d6c370193deed7
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-29 01:21:04 +0600

    ✅  Added challenge Day#24

---
+++ Day #24.js
@@ -0,0 +1,9 @@
+// Write Number in Expanded Form
+/*
+You will be given a number and you will need to return it as a string in Expanded Form.
+*/
+
+// Test Cases
+expandedForm(12); // should return '10+2'
+expandedForm(42); // should return '40+2'
+expandedForm(734); // should return '700+30+4'

06e316e25030fdb13a7838032f0e247b660e21d5
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-29 23:58:39 +0600

    ✅  Updated challenge Day#25

---
+++ Day #25.js
@@ -0,0 +1,9 @@
+// Stop gninnipS My sdroW!
+/*
+Write a function that takes in a string of one or more words, and returns the same string, but with all five or more letter words reversed (Just like the name of this Kata). Strings passed in will consist of only letters and spaces. Spaces will be included only when more than one word is present.
+*/
+
+// Test Cases
+spinWords("Hey fellow warriors"); // should return"Hey wollef sroirraw"
+spinWords("You are almost to the last test"); // should return "You are tsomla to the last test"
+spinWords("Seriously this is the last one"); // should return "ylsuoireS this is the last one"

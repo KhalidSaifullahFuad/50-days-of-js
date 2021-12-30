91e18e13f0124c5a2791b4a7b88b4b1f6577a1a7
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-31 01:35:48 +0600

    ✅ Updated challenge Day#32

---
+++ Day #32.js
@@ -0,0 +1,10 @@
+// Write a program to find the greatest common divisor (gcd) of two positive numbers.
+/*
+The greatest common divisor (GCD), also called the greatest common factor, of two numbers is the largest number that divides them both. For instance, the greatest common factor of 20 and 15 is 5, since 5 divides both 20 and 15 and no larger number has this property.
+*/
+
+
+// Test Cases
+gcd(2154, 458)
+gcd(12, 4)
+gcd(333, 3333)

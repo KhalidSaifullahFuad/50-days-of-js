6f67e034b1553f42778cbca9940f9244121abb84
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-31 01:45:49 +0600

    ✅ Updated challenge Day#34

---
+++ Day #34.js
@@ -0,0 +1,12 @@
+// Write a program to find the most frequent item of an array
+/*
+Input:
+const arr = [3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3];
+
+Output:
+a 5
+*/
+
+// Test Cases
+mostFreq([1, 2, 2, 4, 5, 6, 6]); // returns 2 2
+mostFreq([3, 'a', 'a', 'a', 2, 3, 'a', 3, 'a', 2, 4, 9, 3]); // returns a 5

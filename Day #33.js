c25bbeabc12ce55354d0674444cfb29a150a59b3
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2021-12-31 01:44:04 +0600

    ✅ Updated challenge Day#33

---
+++ Day #33.js
@@ -0,0 +1,13 @@
+// Write a program to print unique values from an array
+/*
+Input:
+const arrOfNum = [1, 2, 2, 4, 5, 6, 6]
+
+Output:
+[1, 2, 4, 5, 6]
+*/
+
+
+// Test Cases
+set([1, 2, 2, 4, 5, 6, 6]); // returns [1, 2, 4, 5, 6]
+set([1, 1, 1, 1, 1]); // returns [1]

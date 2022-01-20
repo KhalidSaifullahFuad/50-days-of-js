c17b64e680c28967ea8a1836877ec73c01af44a7
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-01-20 11:02:10 +0600

    ⚰️ Remove unneccesary code

--- Day #05.js
+++ Day #05.js
@@ -3,13 +3,6 @@
 * indexOf and lastIndexOf are the methods on String which returns the position of the given string in the input string from start and end respectively.
 * If the match is not found, these methods return -1.
 */
-/*
-Test Cases:
-* getTheGapX('XeroX') returns 4
-* getTheGapX('Xamarin') returns 0
-* getTheGapX('JavaScript') returns -1
-* getTheGapX("F(X) !== G(X) !== F(X)") returns 18
-*/
 
 // Solution
 function getTheGapX(str) {

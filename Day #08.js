72d09beaa252bc48f30618de413f19f693112c85
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-01-23 15:05:48 +0600

    ✨ Solve Day#8

--- Day #08.js
+++ Day #08.js
@@ -4,7 +4,12 @@
 * Time difference can be converted in to days by dividing the 24Hrs time in milliseconds
 */
 
+// Solution
+function getDaysBetweenDates(date1, date2) {
+    return Math.abs(new Date(date1).getTime() - new Date(date2).getTime()) / (1000 * 3600 * 24);
+}
+
 // Test Cases
-getDaysBetweenDates('10/15/2020', '12/1/2020'); // returns 47
-getDaysBetweenDates('11/10/2021', '11/12/2021'); // returns 2
-getDaysBetweenDates('11/01/2020', '11/05/2020'); // returns 4
+console.log(getDaysBetweenDates('11/01/2020', '11/05/2020')); // returns 4
+console.log(getDaysBetweenDates('11/10/2021', '11/12/2021')); // returns 2
+console.log(getDaysBetweenDates('10/15/2020', '12/1/2020')); // returns 47

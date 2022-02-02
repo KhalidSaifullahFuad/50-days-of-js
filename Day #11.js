9a22b5453de453c3066b2f82950e4186f2b40f95
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-02-02 11:37:46 +0600

    🌟 Solved Day#11 challenge

--- Day #11.js
+++ Day #11.js
@@ -3,5 +3,11 @@
 * Return the N-th value of the Fibonacci sequence
 */
 
+// Solution
+function fibonacci(n) {
+    return (n<=1) ? n : fibonacci(n - 1) + fibonacci(n - 2);
+}
+
+
 // Test Cases
-function fibonacci(n); // returns the N-th value of the Fibonacci sequence
+console.log(fibonacci(5)); // returns the N-th value of the Fibonacci sequence

9f28640d91c79ae539bf2d1f157909da39455dd8
Khalid Saifullah Fuad <KhalidSaifullahFuad@gmail.com>
2022-02-06 23:19:56 +0600

    ✨ Solved Day#13

--- Day #13.js
+++ Day #13.js
@@ -22,6 +22,29 @@ Your spaceAge function should return the (already given) yearsInAllPlanets objec
 THE VALUE OF EACH PROPERTY SHOULD BE A NUMBER AND SHOULD HAVE MAXIMUM 2 DIGITS AFTER THE POINT example 4.34
 */
 
+// Solution
+
+function spaceAge(seconds){
+    const earthYear = 31557600;
+    const orbitalPeriods = {
+        "Mercury": 0.2408467,
+        "Venus": 0.61519726,
+        "Earth": 1,
+        "Mars": 1.8808158,
+        "Jupiter": 11.862615,
+        "Saturn": 29.447498,
+        "Uranus": 84.016846,
+        "Neptune": 164.79132
+    }
+    const yearsInAllPlanets = {};
+
+    for (let planet in orbitalPeriods) {
+        yearsInAllPlanets[planet] = ((seconds/earthYear)/orbitalPeriods[planet]).toFixed(2);
+    }
+
+    return yearsInAllPlanets;
+}
+
 // Test Cases
-spaceAge(436575687); // should return { "Mercury": 57.44, "Venus": 22.49, "Earth": 13.83, "Mars": 7.36, "Jupiter": 1.17, "Saturn": 0.47, "Uranus": 0.16, "Neptune": 0.08 }
-spaceAge(65965561); // should return { 'Mercury': 8.68 'Venus': 3.4 'Earth': 2.09 'Mars': 1.11 'Jupiter': 0.18 'Saturn': 0.07 'Uranus': 0.02 'Neptune': 0.01 }
+console.log(spaceAge(436575687)); // should return { "Mercury": 57.44, "Venus": 22.49, "Earth": 13.83, "Mars": 7.36, "Jupiter": 1.17, "Saturn": 0.47, "Uranus": 0.16, "Neptune": 0.08 }
+console.log(spaceAge(3407706)); // should return { 'Mercury': 8.68 'Venus': 3.4 'Earth': 2.09 'Mars': 1.11 'Jupiter': 0.18 'Saturn': 0.07 'Uranus': 0.02 'Neptune': 0.01 }
